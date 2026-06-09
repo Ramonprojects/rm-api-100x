2e43effd2675b7be84cf5f6f5c8936c558180adc
Ramonprojects <adm@ramonmago.com>
2026-06-09 16:03:36 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=QM37GAS1NGI&afp=tapic" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio&shareCode=QM37GAS1NGI&afp=tapic" });
     res.end();
   }
